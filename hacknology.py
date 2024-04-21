f86564d951ca30cc08a057bdfbed8aedd660ad99
Holesum <feelthebreeze345@gmail.com>
2024-04-20 23:05:03 -0400

    I just wanna commit man

---
+++ hacknology.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+import pandas as pd
+
+df = pd.read_csv('anime-dataset-2023.csv')
+
+print(df.to_string())
+
+print("would you like to use your MyAnimeLit watch data? (y/n)")
+inputVal = input()
+if(inputVal == 'y'):
+    dothis
+else: 
+    dothat
+foo = 0
+#for(i in range(18)):
+ #   for(j in range(18)):
+  #      if(!genresOfAnime.contains(i)&&!genresOfAnime.contains(j)):
+   #         continue
+    #    foo += array[j][i]
+#genreScore = 1 + foo/(number of top genres) 
+
+LengthScore = 0
+likeness = genreScore + LengthScore
