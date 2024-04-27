62800e399194624c1e1c37a0fb3266e058ab1cd7
Alex-Pat1 <apateroulakis1@pride.hofstra.edu>
2024-04-26 20:18:04 -0400

    Made keyWord A LOT more accurate

--- keywordAnalysis.py
+++ keywordAnalysis.py
@@ -3,6 +3,13 @@ import re
 from gensim.corpora import Dictionary
 from gensim.models import LdaModel
 from gensim.similarities import MatrixSimilarity
+import nltk
+from nltk.corpus import stopwords
+from nltk import pos_tag
+
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+stopwords = set(stopwords.words('english'))
 
 def get_anime_id(anime_df,show_name):
         flag = True
@@ -64,14 +71,29 @@ def keyword_analysis(pop_df, anime_df, anime_id):
     # Tokenize the synopses
     tokenized_synopses_tv = [synopsis.split() for synopsis in synopses_tv]
 
+    tokenized_synopses_no_stopwords = [[token for token in synopsis if token.lower() not in stopwords] for synopsis in tokenized_synopses_tv]
+
+    # POS Tagging
+    tokenized_synopses_pos = [pos_tag(tokens) for tokens in tokenized_synopses_no_stopwords]
+    
+    # Keep tokens that are nouns, verbs, adjectives, and adverbs
+    allowed_pos = {'NN', 'NNS', 'NNP', 'NNPS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS'}
+    tokenized_synopses_filtered = [[token for token, pos in tagged_tokens if pos in allowed_pos] for tagged_tokens in tokenized_synopses_pos]
+    
+    # Remove tokens that are too short or too long
+    tokenized_synopses_filtered = [[token for token in synopsis if 2 <= len(token) <= 15] for synopsis in tokenized_synopses_filtered]
+    
+    # Remove tokens containing non-alphabetical characters or digits
+    tokenized_synopses_filtered = [[token for token in synopsis if token.isalpha()] for synopsis in tokenized_synopses_filtered]
+    
     # Create a dictionary representation of the synopses
-    dictionary_tv = Dictionary(tokenized_synopses_tv)
+    dictionary_tv = Dictionary(tokenized_synopses_filtered)
 
     # Filter out tokens that appear in less than 10 documents or more than 50% of the documents
     dictionary_tv.filter_extremes(no_below=10, no_above=0.5)
 
     # Convert the tokenized synopses into bag-of-words format
-    corpus_tv = [dictionary_tv.doc2bow(synopsis) for synopsis in tokenized_synopses_tv]
+    corpus_tv = [dictionary_tv.doc2bow(synopsis) for synopsis in tokenized_synopses_no_stopwords]
     print("beginning analysis")
 
     # Build the LDA model
